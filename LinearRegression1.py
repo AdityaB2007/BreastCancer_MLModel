73be7f9a85883118ac7696d89a9c820eb8afbbc6
Aditya B <110244326+AdityaB2007@users.noreply.github.com>
2023-11-18 14:35:30 -0600

    Create LinearRegression1.py

---
+++ LinearRegression1.py
@@ -0,0 +1,35 @@
+from bioinfokit.analys import stat, get_data
+import numpy as np
+import pandas as pd
+
+df = get_data('CancerData.csv').data
+print(df)
+
+X = df['']
+y = df['']
+
+from sklearn.linear_model import LinearRegression
+
+X = np.array(X).reshape(-1, 1)
+y = np.array(y)
+reg = LinearRegression().fit(X, y)
+
+# find the slope of best fit line
+print(reg.coef_)
+
+# find y intercept
+print(reg.intercept_)
+
+'''
+predict target variable (yhat) at a given X value
+reg.predict([[X]])
+'''
+
+import statsmodels.api as sm
+
+# add intercept
+X = sm.add_constant(X)
+
+# fit the model
+reg = sm.OLS(y, X).fit()
+print(reg.summary())
